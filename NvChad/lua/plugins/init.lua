53bc3560d52cc01183682b895b2e21afe97cf053
Ivan Luchev <luchevz@gmail.com>
2024-09-23 13:24:34 +0200

    Add nvim surround

--- NvChad/lua/plugins/init.lua
+++ NvChad/lua/plugins/init.lua
@@ -255,6 +255,16 @@ local default_plugins = {
     },
   },
   {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+  {
     "ray-x/go.nvim",
     dependencies = {  -- optional packages
       "ray-x/guihua.lua",
