02db2ade525a636319fb2dc47f72087220aabb11
Ivan Luchev <luchevz@gmail.com>
2025-01-15 11:28:37 +0100

    Add env vars for go development for uber

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -3,7 +3,10 @@ require "nvchad.options"
 local env = vim.env
 local opt = vim.opt
 
+-- Required so vim bypasses the go wrapper from Uber
 env.USE_SYSTEM_GO = 1
+-- Vim uses the go binary provided from bazel if it's set
+env.PATH = env.VIM_PATH or env.PATH
 
 opt.spell = true
 opt.shell = "nu"
