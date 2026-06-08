bc1ff1494b7eb21788866403e334d9fb5c813269
Ivan Luchev <luchevz@gmail.com>
2026-06-08 12:33:49 +0000

    Add wezterm ssh icon

--- wezterm.lua
+++ wezterm.lua
@@ -17,6 +17,26 @@ config.scrollback_lines = 10000
 config.term = 'xterm-256color'
 config.default_prog = { '/opt/homebrew/bin/nu', '--config', '~/.dotfiles/nushell/config.nu', '--env-config', '~/.dotfiles/nushell/env.nu' }
 config.audible_bell = 'Disabled'
+config.status_update_interval = 5000
+
+local function is_ssh_active(pane)
+  local process_name = pane:get_foreground_process_name()
+  if process_name and process_name:lower():find("ssh") then
+    return true
+  end
+  local user_vars = pane:get_user_vars()
+  return user_vars.SSH_MOCK == "1"
+end
+
+wezterm.on('update-status', function(window, pane)
+  if is_ssh_active(pane) then
+    window:set_right_status(wezterm.format {
+      { Text = ' 󰣀 ' },
+    })
+  else
+    window:set_right_status('')
+  end
+end)
 
 return config
 
